89589cc7b5699571a590dba3c37da2017fee09b9
Amorob <amoro.bagei@amalitech.com>
2021-11-23 14:40:23 -0800

    Add email validation

--- models/users.js
+++ models/users.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose")
-    // userSchema
+const { isEmail } = require("validator")
+
+
+// userSchema
 const userSchema = {
     firstName: {
         type: String,
-        require: true
+        require: [true, "Please enter your first name"]
     },
     lastName: {
         type: String,
-        require: true
+        require: [true, "Please enter your last name"]
     },
     email: {
         type: String,
-        require: true,
-        unique: true
+        require: [true, "Please enter your email"],
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, "Please enter a valid email"]
     },
     password: {
         type: String,
-        require: true
+        require: [true, "Please enter your password"],
+        minlength: [8, "Please password can not be less 8 characters"]
     },
     role: {
         type: String,
